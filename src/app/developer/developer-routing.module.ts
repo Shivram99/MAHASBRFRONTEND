c9464914e2120e6711500c54eee3b91daa474b74
Sudhir6re <sshahare2018@gmail.com>
2024-06-08 18:38:41 +0530

    menu

--- src/app/developer/developer-routing.module.ts
+++ src/app/developer/developer-routing.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from '../auth.guard';
 import { MenuComponent } from '../component/menu/menu.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { DeveloperDashboardComponent } from '../component/developer-dashboard/developer-dashboard.component';
 
 
 
 
 const routes: Routes = [
   {path:"menu",component:MenuComponent,canActivate: [authGuard], data: { expectedRole: ['ROLE_DEVELOPER'] } },
-  {path:"developerDashboard",component:MenuComponent,canActivate: [authGuard], data: { expectedRole: ['ROLE_DEVELOPER'] } }
+  {path:"developerDashboard",component:DeveloperDashboardComponent,canActivate: [authGuard], data: { expectedRole: ['ROLE_DEVELOPER'] } }
       
  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule,ReactiveFormsModule]
+  exports: [RouterModule,]
 })
 export class DeveloperRoutingModule { }
