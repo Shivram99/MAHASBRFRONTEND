76416224dfb7d35399699ea61748d18a6af0f41d
JitendraGhasle2506 <jitendraghasle.egov@gmail.com>
2025-11-27 18:54:26 +0530

    config the server ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://13.204.181.90:8085/MAHASBR',
-    apiUrl: 'http://localhost:8085/MAHASBR',
+    apiUrl: 'http://13.204.181.90:8085/MAHASBR',
+    // apiUrl: 'http://localhost:8085/MAHASBR',
    };
