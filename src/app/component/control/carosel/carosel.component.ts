8a6d859c5d57ec439cf21b6940675521890a2b07
JitendraGhasle2506 <jitendraghasle.egov@gmail.com>
2026-05-13 16:39:45 +0530

    feat: implement homepage component with visit tracking and post-login redirection logic

--- src/app/component/control/carosel/carosel.component.ts
+++ src/app/component/control/carosel/carosel.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-carosel',
     templateUrl: './carosel.component.html',
     styleUrl: './carosel.component.css',
     standalone: false
 })
-export class CaroselComponent     {
+export class CaroselComponent {
+  @Input() images: readonly string[] = [
+    'assets/images/slide/img.png',
+    'assets/images/slide/img2.png',
+    'assets/images/slide/img3.png'
+  ];
 
-  
+  trackByImage(_index: number, image: string): string {
+    return image;
+  }
 }
